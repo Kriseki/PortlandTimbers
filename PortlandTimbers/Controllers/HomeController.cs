53c1461080001cc571ca58402a0c65de7cc58f19
Kris <kriseki22@gmail.com>
2018-11-17 16:41:40 -0800

    Initial commit

---
+++ PortlandTimbers/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using PortlandTimbers.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PortlandTimbers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string connectionString = @"Data Source=STUDENT-PC\SQLEXPRESS;Initial Catalog=Timbers;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Roster()
+        {
+            string queryString = @"SELECT LastName, FirstName, Nationality, Position, Number from RosterSheet";
+
+            List<Player> ptfcRoster = new List<Player>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var roster = new Player();
+                    roster.LastName = reader["LastName"].ToString();
+                    roster.FirstName = reader["FirstName"].ToString();
+                    roster.Nationality = reader["Nationality"].ToString();
+                    roster.Position = reader["Position"].ToString();
+                    roster.Number = Convert.ToInt32(reader["Number"]);
+                    ptfcRoster.Add(roster);
+                }
+            }
+
+                return View(ptfcRoster);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ PortlandTimbers/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PortlandTimbers.Models
+{
+    public class Player
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Nationality { get; set; }
+        public string Position { get; set; }
+        public int Number { get; set; }
+    }
+}
